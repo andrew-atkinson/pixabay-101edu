96302478726a859bcaaa8113cb81a10a44efeb78
andrew-atkinson <blind.sam@gmail.com>
2018-02-18 21:07:30 -0500

    feat: adds a searchScreen and binds to store

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// redux set up
+import {Provider} from 'react-redux'
+import configureStore from './src/store/config'
+const store = configureStore();
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Pixabay</Text>
-      </View>
-    );
+//screen set up
+import {Navigation} from 'react-native-navigation'
+import SearchScreen from './src/screens/SearchScreen'
+
+//register screens
+Navigation.registerComponent("pixabay.SearchScreen", () => SearchScreen, store, Provider)
+
+//start app
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "pixabay.SearchScreen",
+    title: "Image Search"
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+})
