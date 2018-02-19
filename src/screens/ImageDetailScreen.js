6a1962a7b0628bd8afd02d32371a99631c0fa7c2
andrew-atkinson <blind.sam@gmail.com>
2018-02-19 16:04:39 -0500

    adds: ImageDetailScreen component

---
+++ src/screens/ImageDetailScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, Text, Image} from 'react-native'
+
+export default ImageDetailScreen = props => {
+  const [currentImage] = props.currentImage
+  console.log('currentImage', currentImage)
+  return (
+    <View>
+      <Image resizeMode='contain' 
+             style={{width:"100%", height:"100%"}} 
+             source={{uri: currentImage.webformatURL}}
+      />
+    </View>
+  )
+}
