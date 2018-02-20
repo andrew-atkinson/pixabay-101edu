d7ed70636e31dce067e77532e0304bd6bf5031e7
andrew-atkinson <blind.sam@gmail.com>
2018-02-19 22:33:43 -0500

    fixes: one search concat to another, additional cases in the reducer

--- src/store/reducers/getImageQuery.js
+++ src/store/reducers/getImageQuery.js
@@ -4,17 +4,27 @@ import KEY from '../../../apikey'
 /* -----------------    ACTIONS     ------------------ */
 
 const GET_IMAGES = 'GET_IMAGES'
+const GET_MORE_IMAGES = 'GET_MORE_IMAGES'
+
 
 /* ------------   ACTION CREATORS     ------------------ */
 
 const get = (query) => ({type: GET_IMAGES, query})
+const getMore = (query) => ({type: GET_MORE_IMAGES, query})
 
 /* ------------       REDUCER     ------------------ */
 
-export default function reducer(query = {hits: [], page: 1}, action) {
+export default function reducer(query = {hits: [],  page: 1}, action) {
   switch (action.type) {
     case GET_IMAGES:
       return {
+        ...query, 
+        total: action.query.total,
+        hits: action.query.hits
+      }
+    case GET_MORE_IMAGES:
+      return {
+        ...query, 
         total: action.query.total,
         hits: query.hits.concat(action.query.hits),
         page: action.query.page
@@ -27,8 +37,15 @@ export default function reducer(query = {hits: [], page: 1}, action) {
 /* ------------   THUNK CREATORS     ------------------ */
 
 export const getImageQueryResults = query => dispatch => {
+  axios.get(`https://pixabay.com/api/?key=${KEY}&q=${encodeURI(query.text)}&webformatURL='_960'&per_page=20&page=1`)
+  .then(res => {
+    dispatch(get({hits: res.data.hits, total: res.data.total}))
+  })
+}
+
+export const getMoreImageQueryResults = query => dispatch => {
   axios.get(`https://pixabay.com/api/?key=${KEY}&q=${encodeURI(query.text)}&webformatURL='_960'&per_page=20&page=${query.page}`)
   .then(res => {
-    dispatch(get({hits:res.data.hits, total:res.data.total, page:query.page}))
+    dispatch(getMore({hits: res.data.hits, total: res.data.total, page: query.page}))
   })
 }
