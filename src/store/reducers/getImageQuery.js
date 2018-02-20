06e93f22dba084802ca36a187016616508e565a1
andrew-atkinson <blind.sam@gmail.com>
2018-02-20 08:03:51 -0500

    chore: changed some variables for clarity

--- src/store/reducers/getImageQuery.js
+++ src/store/reducers/getImageQuery.js
@@ -9,28 +9,28 @@ const GET_MORE_IMAGES = 'GET_MORE_IMAGES'
 
 /* ------------   ACTION CREATORS     ------------------ */
 
-const get = (query) => ({type: GET_IMAGES, query})
-const getMore = (query) => ({type: GET_MORE_IMAGES, query})
+const getImages = (query) => ({type: GET_IMAGES, query})
+const getMoreImages = (query) => ({type: GET_MORE_IMAGES, query})
 
 /* ------------       REDUCER     ------------------ */
 
-export default function reducer(query = {hits: [],  page: 1}, action) {
+export default function reducer(state = {hits: [],  page: 1}, action) {
   switch (action.type) {
     case GET_IMAGES:
       return {
-        ...query, 
+        ...state, 
         total: action.query.total,
         hits: action.query.hits
       }
     case GET_MORE_IMAGES:
       return {
-        ...query, 
+        ...state, 
         total: action.query.total,
-        hits: query.hits.concat(action.query.hits),
+        hits: state.hits.concat(action.query.hits),
         page: action.query.page
       }
     default:
-      return query
+      return state
   }
 }
 
@@ -39,13 +39,13 @@ export default function reducer(query = {hits: [],  page: 1}, action) {
 export const getImageQueryResults = query => dispatch => {
   axios.get(`https://pixabay.com/api/?key=${KEY}&q=${encodeURI(query.text)}&webformatURL='_960'&per_page=20&page=1`)
   .then(res => {
-    dispatch(get({hits: res.data.hits, total: res.data.total}))
+    dispatch(getImages({hits: res.data.hits, total: res.data.total}))
   })
 }
 
 export const getMoreImageQueryResults = query => dispatch => {
   axios.get(`https://pixabay.com/api/?key=${KEY}&q=${encodeURI(query.text)}&webformatURL='_960'&per_page=20&page=${query.page}`)
   .then(res => {
-    dispatch(getMore({hits: res.data.hits, total: res.data.total, page: query.page}))
+    dispatch(getMoreImages({hits: res.data.hits, total: res.data.total, page: query.page}))
   })
 }
