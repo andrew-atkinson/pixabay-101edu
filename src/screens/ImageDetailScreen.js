e0fdc6d0ba336c57f441fad6fd867a40cef8e97c
andrew-atkinson <blind.sam@gmail.com>
2018-02-19 22:48:52 -0500

    adds: basic data

--- src/screens/ImageDetailScreen.js
+++ src/screens/ImageDetailScreen.js
@@ -5,6 +5,9 @@ export default ImageDetailScreen = props => {
   const [currentImage] = props.currentImage
   return (
     <View>
+      <Text>User Name: {currentImage.user}</Text>
+      <Text>Tags: {currentImage.tags}</Text>
+      <Text>Resolution: {currentImage.imageWidth} x {currentImage.imageHeight}</Text>
       <Image resizeMode='contain' 
              style={{width:"100%", height:"100%"}} 
              source={{uri: currentImage.webformatURL}}
